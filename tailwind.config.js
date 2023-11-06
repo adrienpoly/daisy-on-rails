afe9d767a88e3f23c6c9e754829b4c2efecd829f
Adrien POLY <adrienpoly@gmail.com>
2023-11-06 09:04:55 +0100

    update tailwind conf to account for components

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,27 @@
 module.exports = {
   content: [
     './app/views/**/*.html.erb',
+    './app/components/**/*.html.erb',
+    './app/components/**/*.rb',
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js'
   ],
-  plugins: [require('@tailwindcss/typography'), require('@tailwindcss/forms'), require('daisyui')]
+  theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        sm: '1rem'
+      }
+    }
+  },
+  daisyui: {
+    themes: ['light', 'dark']
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+    require('daisyui')
+  ]
 }
