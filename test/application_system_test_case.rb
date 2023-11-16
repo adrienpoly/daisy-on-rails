d6b992e7b9a460b4c09f6d8adb06760bb4049bd6
Adrien POLY <adrienpoly@gmail.com>
2023-11-16 07:31:46 +0100

    registration form and tests

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -7,7 +7,7 @@ class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
     visit sign_in_url
     fill_in :email, with: user.email
     fill_in :password, with: "Secret1*3*5*"
-    click_on "Sign in"
+    click_on "sign_in"
 
     assert_current_path root_url
     user
--- test/system/registrations_test.rb
+++ test/system/registrations_test.rb
@@ -4,10 +4,9 @@ class RegistrationsTest < ApplicationSystemTestCase
   test "signing up" do
     visit sign_up_url
 
-    fill_in "Email", with: "lazaronixon@hey.com"
-    fill_in "Password", with: "Secret6*4*2*"
-    fill_in "Password confirmation", with: "Secret6*4*2*"
-    click_on "Sign up"
+    fill_in "email", with: "lazaronixon@hey.com"
+    fill_in "password", with: "Secret6*4*2*"
+    click_on "sign_up"
 
     assert_text "Welcome! You have signed up successfully"
   end
