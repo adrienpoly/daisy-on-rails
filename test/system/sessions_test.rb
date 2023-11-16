dd10dd195eaeca8886de92d4a3d098763abcc3c1
Adrien POLY <adrienpoly@gmail.com>
2023-11-16 07:19:46 +0100

    simplify session form and fix session test

--- test/system/sessions_test.rb
+++ test/system/sessions_test.rb
@@ -14,9 +14,9 @@ class SessionsTest < ApplicationSystemTestCase
 
   test "signing in" do
     visit sign_in_url
-    fill_in "Email", with: @user.email
-    fill_in "Password", with: "Secret1*3*5*"
-    click_on "Sign in"
+    fill_in "email", with: @user.email
+    fill_in "password", with: "Secret1*3*5*"
+    click_on "sign_in"
 
     assert_text "Signed in successfully"
   end
