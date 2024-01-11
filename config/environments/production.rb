8a872bbc74184ed267124c5f2f07fc422d8a50e0
Adrien POLY <adrienpoly@gmail.com>
2024-01-11 07:41:24 +0100

    update app name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "shoping_list_production"
+  # config.active_job.queue_name_prefix = "daisy_on_rails_production"
 
   config.action_mailer.perform_caching = false
 
