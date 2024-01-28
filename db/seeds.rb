afe9f1214ad8d15ee74b6bdd8f1d365dd3618b68
Adrien POLY <adrienpoly@gmail.com>
2024-01-28 22:35:24 +0100

    add seed for prod

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,21 @@
-puts "To seed the database, run 'rails db:fixtures:load'"
+if Rails.env.development?
+  puts "deleting all users in develoment environment only"
+  User.destroy_all
+end
+
+puts "Seeding users"
+
+fixture_file_path = Rails.root.join("test", "fixtures", "users.yml")
+
+fixture_content = File.read(fixture_file_path)
+
+interpolated_users = YAML.load(ERB.new(fixture_content).result(binding))
+
+interpolated_users.each do |key, user|
+  User.find_or_initialize_by(email: user["email"]) do |u|
+    u.update!(user)
+  end
+  puts "Seeded user #{user["email"]}"
+end
+
+puts "Seeding users completed"
