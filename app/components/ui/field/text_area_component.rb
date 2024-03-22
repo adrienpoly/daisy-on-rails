e380f12444ee397f143c3dedb811948dfb3ab6f3
Bob Maerten <bob.maerten@gmail.com>
2024-03-22 17:25:30 +0100

    Add wrapper_class option to Ui::Field::Text* components (#14)

--- app/components/ui/field/text_area_component.rb
+++ app/components/ui/field/text_area_component.rb
@@ -4,11 +4,13 @@ class Ui::Field::TextAreaComponent < ApplicationComponent
   renders_one :top_right_label
 
   param :name, optional: true
-  option :form_builder, type: Dry::Types["nominal.any"], optional: true
-  option :label, type: Dry::Types["strict.false"] | Dry::Types["coercible.string"], optional: true
-  option :error, type: Dry::Types["coercible.string"], optional: true
-  option :hint, type: Dry::Types["coercible.string"], optional: true
-  option :value, type: Dry::Types["coercible.string"], optional: true
+  option :form_builder, type: Dry::Types['nominal.any'], optional: true
+  option :label, type: Dry::Types['strict.false'] | Dry::Types['coercible.string'], optional: true
+  option :error, type: Dry::Types['coercible.string'], optional: true
+  option :hint, type: Dry::Types['coercible.string'], optional: true
+  option :value, type: Dry::Types['coercible.string'], optional: true
+  option :class, type: Dry::Types['coercible.string'], optional: true
+  option :wrapper_class, type: Dry::Types['coercible.string'], optional: true
 
   private
 
@@ -20,7 +22,11 @@ class Ui::Field::TextAreaComponent < ApplicationComponent
 
   def field_classes
     classes = attributes.delete(:class)
-    [class_names("textarea textarea-bordered w-full", "input-error": errors), classes].compact.join(" ")
+    [class_names('textarea textarea-bordered w-full', "input-error": errors), classes].compact.join(' ')
+  end
+
+  def wrapper_classes
+    attributes.delete(:wrapper_class)
   end
 
   def errors
