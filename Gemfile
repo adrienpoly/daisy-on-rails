f89eb6256164cd1b429aacc4b416dc2b38dad64a
Adrien POLY <adrienpoly@gmail.com>
2024-05-12 08:21:47 +0200

    upgrade to rails 7.2 alpha

--- Gemfile
+++ Gemfile
@@ -3,7 +3,7 @@ source "https://rubygems.org"
 ruby "3.3.1"
 
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
-gem "rails", "~> 7.1.3.2"
+gem "rails", github: "rails/rails", branch: "main"
 
 # The modern asset pipeline for Rails [https://github.com/rails/propshaft]
 gem "propshaft"
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_13_184444) do
+ActiveRecord::Schema[7.2].define(version: 2024_05_12_061715) do
   create_table "ahoy_events", force: :cascade do |t|
     t.integer "visit_id"
     t.integer "user_id"
